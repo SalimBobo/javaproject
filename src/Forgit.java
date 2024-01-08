dfcd48cd3f6f3b292bc75d4bac78454d0cb802de
JavaScript0205 <javascript0222@gmail.com>
2024-01-08 11:48:44 +0500

    muhammadjon

--- src/Forgit.java
+++ src/Forgit.java
@@ -2,5 +2,7 @@ public class Forgit {
     public static void main(String[] args) {
 
         System.out.println("Salim BoBo");
+
+        System.out.println("Hello Salim Bobo. how are you?");
     }
 }
